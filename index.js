f3c2664a4ebcc00c7bb2abad34728b436b7fcd66
Gee2563 <george.farhatsmith@gmail.com>
2024-04-18 10:42:04 +0100

    added put function on backend so app updates existing entry with new number.
    added error handling function and updated error logging accoridngly
    
    reorganised structure: position of  function definitions and order of middleware

--- index.js
+++ index.js
@@ -3,8 +3,6 @@ const express = require('express')
 const app = express()
 const Person = require('./model/persons')
 
-
-
 app.use(express.static('dist'))
 
 const requestLogger = (request, response, next) => {
@@ -15,9 +13,6 @@ const requestLogger = (request, response, next) => {
   next()
 }
 
-const cors = require('cors')
-
-app.use(cors())
 app.use(express.json())
 app.use(requestLogger)
 
@@ -38,25 +33,16 @@ app.get('/api/persons/:id', (request,response) => {
     .then(person => {
         response.json(person)
     })
-    .catch(error => {
-        console.log(error)
-        response.status(404).end()
-    })
+    .catch(error => next(error))
 })
-
 
 app.delete('/api/persons/:id', (request, response) => {
     Person.findByIdAndDelete(request.params.id)
     .then(result => {
         response.status(204).end()
     })
-    .catch(error => {
-        console.log(error)
-        response.status(404).end()
-    })
+    .catch(error => next(error))
 })
-
-
 
 app.post('/api/persons/', (request, response) => {
     const {name, number} = request.body
@@ -70,10 +56,30 @@ app.post('/api/persons/', (request, response) => {
     person.save().then(savedPerson => {
         response.json(savedPerson)
     })
-
-
 })
 
+app.put('/api/persons/:id', (request, response) =>{
+    const {name, number} = request.body
+    const person = {
+        name: name,
+        number: number
+    }
+    Person.findByIdAndUpdate(request.params.id, person , {new: true})
+    .then (updatedPerson => {
+        response.json(updatedPerson)
+    })
+    .catch(error => next(error))
+})
+
+
+const errorHandler = (error, request, response, next) => {
+    console.log(error.message)
+    if (error.name === 'CastError') {
+        return response.status(400).send({error: 'malformatted id'})
+    } next(error)
+}
+
+app.use(errorHandler)
 
 const PORT = process.env.PORT 
 app.listen(PORT, ()=> {
